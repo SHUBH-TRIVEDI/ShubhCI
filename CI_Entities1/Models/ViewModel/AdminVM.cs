97dc69f1649a1f0759c1a6fc79f0361b36fa15b8
SHUBH-TRIVEDI <trivedishubh21@gmail.com>
2023-04-18 18:11:56 +0530

    Data in Datatable

--- CI_Entities1/Models/ViewModel/AdminVM.cs
+++ CI_Entities1/Models/ViewModel/AdminVM.cs
@@ -24,5 +24,52 @@ namespace CI_Entities1.Models.ViewModel
         public DateTime? UpdatedAt { get; set; }
 
         public DateTime? DeletedAt { get; set; }
+
+        //_User.cshtml
+
+        public List<User> users { get; set; }
+
+        public string? UserFirstName { get; set; }
+
+        public string? UserLastName { get; set; }
+
+        public string? UserEmail { get; set; }
+
+        public string? EmployeeId { get; set; }
+        public string? Department { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        //_AdminMission.cshtml
+
+        public List<Mission> missions { get; set; }
+
+        public string Title { get; set; }
+        public string MissionType { get; set; }
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public string? MissionStatus { get; set; }
+
+        //_StoryAdmin
+        public List<Story> stories { get; set; }
+        public long MissionId { get; set; }
+
+        public long UserId { get; set; }
+
+        public string? StoryTitle { get; set; }
+
+        //_ApplicationAdmin
+        public List<MissionApplication> missionApplications { get; set; }
+
+        public DateTime AppliedAt { get; set; }
+
+        public string ApprovalStatus { get; set; } = null!;
+        
+        public List<MissionTheme> missionThemes { get; set; }
+
+        public List<Skill> skills { get; set; }
+
     }
 }
--- CI_Platform1/Areas/Admin/Controllers/AdminController.cs
+++ CI_Platform1/Areas/Admin/Controllers/AdminController.cs
@@ -31,33 +31,117 @@ namespace CI_Platform1.Areas.Admin.Controllers
             _Landing = landing;
         }
 
-
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult _User()
+        public IActionResult _UserAdmin()
         {
-            UserVM uv = new UserVM();
-            uv.users = _CiPlatformContext.Users.ToList();
-            return PartialView("_User", uv);
+            AdminVM adminVM = new AdminVM();
+            adminVM.users = _CiPlatformContext.Users.ToList();
+
+            return PartialView("_UserAdmin", adminVM);
         }
 
-        public IActionResult User()
+        public IActionResult _MissionAdmin()
         {
-            UserVM uv=new UserVM();
-            uv.users = _CiPlatformContext.Users.ToList();
-            
-            //var user = _CiPlatformContext.Users.FirstOrDefault();
-            //uv.FirstName= user.FirstName;
-            //uv.LastName = user.LastName;
-            //uv.Email= user.Email;
-            ////uv.Department=user.Department;
-            ////uv.EmployeeId=user.EmployeeId;
-            //uv.Status=user.Status;
-            return View(uv);
+            AdminVM adminVM = new AdminVM();
+            adminVM.missions = _CiPlatformContext.Missions.ToList();
+
+            return PartialView("_MissionAdmin", adminVM);
+        }
+
+        public IActionResult _CMSAdmin()
+        {
+            AdminVM adminVM = new AdminVM();
+            adminVM.missions = _CiPlatformContext.Missions.ToList();
+
+            return PartialView("_CMSAdmin", adminVM);
+        }
+
+        //Story
+        public IActionResult _StoryAdmin()
+        {
+            AdminVM adminVM = new AdminVM();
+            adminVM.stories = _CiPlatformContext.Stories.Where(u=> u.Status =="Pending").ToList();
+            adminVM.missions = _CiPlatformContext.Missions.ToList();
+            adminVM.users = _CiPlatformContext.Users.ToList();
+
+            return PartialView("_StoryAdmin", adminVM);
+        }
+
+        public bool ApproveStory(long storyid)
+        {
+            var story = _CiPlatformContext.Stories.FirstOrDefault(u => u.StoryId == storyid);
+            story.Status = "Approved";
+            _CiPlatformContext.Stories.Update(story);
+            _CiPlatformContext.SaveChanges();
+
+            return true;
+        }
+
+        public bool RejectStory(long storyid)
+        {
+            var story = _CiPlatformContext.Stories.FirstOrDefault(u => u.StoryId == storyid);
+
+            story.Status = "Rejected";
+            _CiPlatformContext.Stories.Update(story);
+            _CiPlatformContext.SaveChanges();
+
+            return true;
+        }
+
+        //MissionApplication
+        public IActionResult _Applicationadmin()
+        {
+            AdminVM adminVM = new AdminVM();
+            adminVM.missionApplications = _CiPlatformContext.MissionApplications.Where(u => u.ApprovalStatus == "Pending").ToList();
+            adminVM.missions = _CiPlatformContext.Missions.ToList();
+            adminVM.users = _CiPlatformContext.Users.ToList();
+
+            return PartialView("_Applicationadmin", adminVM);
+        }
+
+        public bool ApproveApplication(long Missionapplicationid)
+        {
+            var missapp = _CiPlatformContext.MissionApplications.FirstOrDefault(u => u.MissionApplicationId == Missionapplicationid);
+            missapp.ApprovalStatus = "Approved";
+
+            _CiPlatformContext.MissionApplications.Update(missapp);
+            _CiPlatformContext.SaveChanges();
+
+            return true;
+        }
+
+        public bool RejectApplication(long Missionapplicationid)
+        {
+            var missapp = _CiPlatformContext.MissionApplications.FirstOrDefault(u => u.MissionApplicationId == Missionapplicationid);
+            missapp.ApprovalStatus = "Rejected";
+
+            _CiPlatformContext.MissionApplications.Update(missapp);
+            _CiPlatformContext.SaveChanges();
+
+            return true;
+        }
+
+        //Theme
+
+        public IActionResult _ThemeAdmin()
+        {
+            AdminVM adminVM = new AdminVM();
+            adminVM.missionThemes= _CiPlatformContext.MissionThemes.ToList();
+
+            return PartialView("_ThemeAdmin", adminVM);
+        }
+
+        //Skill
+        public IActionResult _SkillAdmin()
+        {
+            AdminVM adminVM = new AdminVM();
+            adminVM.skills = _CiPlatformContext.Skills.ToList();
+
+            return PartialView("_SkillAdmin", adminVM);
         }
     }
 }
